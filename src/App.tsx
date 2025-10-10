311c11490ecf4b4355bfeccb170a4ae180c8f42f
Sortjakke <erick.matheusrs1003@gmail.com>
2025-10-10 17:50:24 -0300

    chore: setup project with Tailwind, Vitest, and Font Awesome

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { faSun } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+function App() {
+  return <h1 className="text-blue-900 text-3xl font-bold underline">Hello world! <FontAwesomeIcon icon={faSun}/></h1>
+}
+
+export default App
